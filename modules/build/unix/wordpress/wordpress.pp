5965d04e911244cf505a47b51c1758ef39e47e88
Connor Wilson <connorwilson1@googlemail.com>
2016-04-16 14:08:12 +0100

    Fixes networking issues

--- modules/build/unix/wordpress/wordpress.pp
+++ modules/build/unix/wordpress/wordpress.pp
@@ -1,8 +1,28 @@
+include lamp
+
+exec { 'add_sql':
+  command => 'apt-get --yes --force-yes install php5-mysql',
+  provider => 'shell'
+  # path    => [ '/usr/local/bin/', '/bin/' ],  # alternative syntax
+}
+
 class { 'wordpress':
   install_dir => '/var/www/wordpress',
   db_user => 'root',
   db_password => '',
-  notify => File['wp_cli_script']
+  notify => File['wordpress_cli']
+}
+
+
+file{
+  'wordpress_cli':
+    ensure => 'present',
+    source => 'puppet:///modules/wordpress/wp-cli.phar',
+    path => '/var/www/wordpress/wp-cli.phar',
+    owner => 'root',
+    group => 'root',
+    mode => '0744',
+    notify => File['wp_cli_script']
 }
 
 file{
@@ -18,6 +38,6 @@ file{
 
 exec {
   'run_wp_cli':
-    command => '/var/www/wordpress/wpcli-install.sh',
+    command => '/var/www/wordpress/wpcli-install.sh'
 
 }

f1ea5fa70b652757340cdee0209eed5ca51583b4
girlier <roryjfletch@gmail.com>
2026-04-29 21:54:25 +0100

    Add permission control to cron_tar_wildcard module
    - Default: world-writable (0777) for backup/archive directories
    - Optional restrict_write_user parameter limits write access to specific user
    - User validation via exec ensures user exists before applying restrictions
    - cron_user parameter allows non-root cron execution
    - Clear permission hierarchy logging via notice statements
    - Leak storage directory adjusts based on cron_user

--- modules/vulnerabilities/unix/access_control_misconfigurations/cron_tar_wildcard/manifests/config.pp
+++ modules/vulnerabilities/unix/access_control_misconfigurations/cron_tar_wildcard/manifests/config.pp
@@ -7,8 +7,19 @@ class cron_tar_wildcard::config {
     undef   => '/opt',
     default => $secgen_parameters['cron_location'],
   }
+  $cron_user = $secgen_parameters['cron_user'] ? {
+    undef   => 'root',
+    default => $secgen_parameters['cron_user'],
+  }
+  $restrict_write_user_input = $secgen_parameters['restrict_write_user']
 
   $backup_dir = "${cron_location}/backup"
+  $archive_dest = '/var/spool/backups'
+
+  $leak_storage_dir = $cron_user ? {
+    'root'  => '/root',
+    default => "/home/${cron_user}",
+  }
 
   package { 'cron':
     ensure => installed,
@@ -20,22 +31,68 @@ class cron_tar_wildcard::config {
     require => Package['cron'],
   }
 
-  file { '/var/spool/backups':
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-  }
+  if $restrict_write_user_input and $restrict_write_user_input != '' {
+    $restrict_write_user = $restrict_write_user_input
+    $backup_dir_owner = $restrict_write_user
+    $backup_dir_group = $restrict_write_user
+    $backup_dir_mode = '0755'
+    $archive_owner = $restrict_write_user
+    $archive_group = $restrict_write_user
+    $archive_mode = '0755'
 
-  file { $backup_dir:
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0777',
+    exec { "validate_restricted_user":
+      command => "/usr/bin/id ${restrict_write_user}",
+      unless  => "/usr/bin/id ${restrict_write_user}",
+      path    => ['/usr/bin'],
+      before  => File[$backup_dir],
+    }
+
+    notice("Permission hierarchy: Write access restricted to user '${restrict_write_user}'")
+    notice("Backup directory mode: 0755 (user-writable only)")
+    notice("Archive directory mode: 0755 (user-writable only)")
+
+    file { $archive_dest:
+      ensure  => directory,
+      owner   => $archive_owner,
+      group   => $archive_group,
+      mode    => $archive_mode,
+    }
+
+    file { $backup_dir:
+      ensure  => directory,
+      owner   => $backup_dir_owner,
+      group   => $backup_dir_group,
+      mode    => $backup_dir_mode,
+      require => Exec['validate_restricted_user'],
+    }
+  } else {
+    $backup_dir_owner = 'root'
+    $backup_dir_group = 'root'
+    $backup_dir_mode = '0777'
+    $archive_owner = 'root'
+    $archive_group = 'root'
+    $archive_mode = '0777'
+
+    notice("Permission hierarchy: Write access global (mode 0777)")
+    notice("Backup directory mode: 0777 (world-writable)")
+    notice("Archive directory mode: 0777 (world-writable)")
+
+    file { $archive_dest:
+      ensure => directory,
+      owner  => $archive_owner,
+      group  => $archive_group,
+      mode   => $archive_mode,
+    }
+
+    file { $backup_dir:
+      ensure => directory,
+      owner  => $backup_dir_owner,
+      group  => $backup_dir_group,
+      mode   => $backup_dir_mode,
+    }
   }
 
   if $show_crontab_hint =~ /^(true|1|yes)$/ {
-    # Enable sudo access to crontab -l so players can discover the cron job
     class { 'sudo':
       config_file_replace => false,
     }
@@ -55,7 +112,7 @@ class cron_tar_wildcard::config {
       owner   => 'root',
       group   => 'root',
       mode    => '0644',
-      content => "# cron backup hint\n# Try: sudo -l to see what you can run\n# Then: sudo crontab -l to view root's cron jobs\n(cd ${backup_dir} && tar -zcf /var/spool/backups/backup.tar.gz *)\n",
+      content => "# cron backup hint\n# Try: sudo -l to see what you can run\n# Then: sudo crontab -l to view root's cron jobs\n(cd ${backup_dir} && tar -zcf ${archive_dest}/backup.tar.gz *)\n",
     }
   } else {
     file { "${cron_location}/backup.txt":
@@ -63,23 +120,23 @@ class cron_tar_wildcard::config {
       owner   => 'root',
       group   => 'root',
       mode    => '0644',
-      content => "# cron backup hint\n(cd ${backup_dir} && tar -zcf /var/spool/backups/backup.tar.gz *)\n",
+      content => "# cron backup hint\n(cd ${backup_dir} && tar -zcf ${archive_dest}/backup.tar.gz *)\n",
     }
   }
 
   cron { 'backup_wildcard':
-    command => "cd ${backup_dir} && tar -zcf /var/spool/backups/backup.tar.gz *",
-    user    => 'root',
+    command => "cd ${backup_dir} && tar -zcf ${archive_dest}/backup.tar.gz *",
+    user    => $cron_user,
     hour    => '*',
     minute  => '*',
-    require => [Service['cron'], File[$backup_dir]],
+    require => [Service['cron'], File[$backup_dir], File[$archive_dest]],
   }
 
   ::secgen_functions::leak_files { 'cron_tar_wildcard-file-leak':
-    storage_directory => '/root',
+    storage_directory => $leak_storage_dir,
     leaked_filenames  => $leaked_filenames,
     strings_to_leak   => $strings_to_leak,
-    owner             => 'root',
+    owner             => $cron_user,
     mode              => '0600',
     leaked_from       => 'cron_tar_wildcard',
   }

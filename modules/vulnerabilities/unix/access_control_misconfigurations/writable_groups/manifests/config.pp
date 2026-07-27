55693521028bea051fa6249713061a11a32a1136
alixthegreat <hiscockalix@gmail.com>
2026-07-27 13:45:37 +0100

    Add strings_to_leak to writable_groups

--- modules/vulnerabilities/unix/access_control_misconfigurations/writable_groups/manifests/config.pp
+++ modules/vulnerabilities/unix/access_control_misconfigurations/writable_groups/manifests/config.pp
@@ -1,6 +1,19 @@
 class writable_groups::config {
+  $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
+  $strings_to_leak  = $secgen_parameters['strings_to_leak']
+  $leaked_filenames = $secgen_parameters['leaked_filenames']
+
   file { '/etc/group':
     ensure  => present,
     mode    => '0777',
+  }
+
+  ::secgen_functions::leak_files { 'writable-groups-flag-leak':
+    storage_directory => '/root',
+    leaked_filenames  => $leaked_filenames,
+    strings_to_leak   => $strings_to_leak,
+    owner             => 'root',
+    mode              => '0600',
+    leaked_from       => 'writable_groups',
   }
 }

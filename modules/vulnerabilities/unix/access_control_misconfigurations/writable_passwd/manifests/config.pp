9d8316e204c28f61cb5ffd7138167ce028755cf2
alixthegreat <hiscockalix@gmail.com>
2026-07-27 13:39:19 +0100

    Add strings_to_leak to writable_passwd

--- modules/vulnerabilities/unix/access_control_misconfigurations/writable_passwd/manifests/config.pp
+++ modules/vulnerabilities/unix/access_control_misconfigurations/writable_passwd/manifests/config.pp
@@ -1,6 +1,19 @@
 class writable_passwd::config {
+  $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
+  $strings_to_leak  = $secgen_parameters['strings_to_leak']
+  $leaked_filenames = $secgen_parameters['leaked_filenames']
+
   file { '/etc/passwd':
     ensure  => present,
     mode    => '0777',
+  }
+
+  ::secgen_functions::leak_files { 'writable-passwd-flag-leak':
+    storage_directory => '/root',
+    leaked_filenames  => $leaked_filenames,
+    strings_to_leak   => $strings_to_leak,
+    owner             => 'root',
+    mode              => '0600',
+    leaked_from       => 'writable_passwd',
   }
 }
